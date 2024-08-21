007642f7c33830432460d85571f088051f05dc96
kv2000in <kv2000inn@gmail.com>
2024-08-21 00:18:57 -0400

    Startup

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+const app = express();
+app.use(bodyParser.json());
+
+const recordsFile = 'records.json';
+
+	// Initialize records file if it doesn't exist
+if (!fs.existsSync(recordsFile)) {
+	fs.writeFileSync(recordsFile, JSON.stringify([]));
+}
+
+app.post('/clock-in', (req, res) => {
+		 const { name, time } = req.body;
+		 const records = JSON.parse(fs.readFileSync(recordsFile));
+		 records.push({ name, time, action: 'clock-in' });
+		 fs.writeFileSync(recordsFile, JSON.stringify(records));
+		 res.send('Clock In recorded');
+		 });
+
+app.post('/clock-out', (req, res) => {
+		 const { name, time } = req.body;
+		 const records = JSON.parse(fs.readFileSync(recordsFile));
+		 records.push({ name, time, action: 'clock-out' });
+		 fs.writeFileSync(recordsFile, JSON.stringify(records));
+		 res.send('Clock Out recorded');
+		 });
+
+app.get('/records', (req, res) => {
+		const records = JSON.parse(fs.readFileSync(recordsFile));
+		res.json(records);
+		});
+
+app.listen(3000, () => {
+		   console.log('Server is running on port 3000');
+		   });
+
