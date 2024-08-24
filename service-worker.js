53463aaf65a0c36e50671ecc76ca16a5c7b7549d
kv2000in <kv2000inn@gmail.com>
2024-08-24 16:14:54 -0400

    missing directory links

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
 const CACHE_NAME = 'clock-in-out-tracker-cache-v1';
 const urlsToCache = [
-					 '/',
-					 '/index.html',
-					 '/manifest.json',
-					 '/icons/icon-192x192.png',
-					 '/icons/icon-512x512.png'
+					 './',
+					 './index.html',
+					 './manifest.json',
+					 './icons/icon-192x192.png',
+					 './icons/icon-512x512.png'
 					 ];
 
 self.addEventListener('install', (event) => {
